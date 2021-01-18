ddce853d8148d04427d5b9dd75c682d80a1740ef
zaharchenko-test <74099400+zaharchenko-test@users.noreply.github.com>
2021-01-18 21:35:31 +0200

    +

--- install-cli.sh
+++ install-cli.sh
@@ -4,10 +4,15 @@ apt update && apt install -y wget proot
 cd ~/../usr/bin/
 
 wget https://raw.githubusercontent.com/zaharchenko-test/termux-cli/master/cli
+
 chmod +x cli
+
 wget https://raw.githubusercontent.com/zaharchenko-test/termux-cli/master/cli-a
+
 chmod +x cli-a
+
 wget https://raw.githubusercontent.com/zaharchenko-test/termux-cli/master/sudo
+
 chmod +x sudo
 
 
